b5f60ed2f280ff5101fcea8e14d96e5c041884ed
Caetano Matos <caetano.matos@thoughtworks.com>
2019-09-11 14:36:45 -0300

    [talita/cae] added subtract implementation

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -7,17 +8,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void onePlusOneReturnsTwo() {
-        Calculator calculator = new Calculator();
-
         assertThat(calculator.sum(1, 1), equalTo(2));
     }
 
     @Test
     public void onePlusTwoReturnsThree() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum(1, 2), equalTo(3));
+    }
+
+    @Test
+    public void twoMinusOneReturnsOne(){
+        assertThat(calculator.subtract(2, 1), equalTo(1));
+    }
+
+    @Test
+    public void threeMinusOneReturnsTwo(){
+        assertThat(calculator.subtract(3, 1), equalTo(2));
     }
 
     // soma e multiplica
